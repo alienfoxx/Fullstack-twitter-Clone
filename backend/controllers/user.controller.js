69d5f19420745cce43f1f928f2532d816d3ae216
alienfoxx <alienfoxx001@gmail.com>
2025-02-11 21:06:30 +0100

    adding the get suggested user functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -64,12 +64,31 @@ export const followUnfollowUser = async (req, res) => {
   }
 };
 
-export const getSuggestedUsers= async (req,res) =>{
-try {
+export const getSuggestedUsers = async (req, res) => {
+  try {
     const userId = req.user._id;
 
-    const usersFollowedByMe = await
-} catch (error) {
-    
-}
-}
+    const usersFollowedByMe = await User.findById(userId).select("following");
+
+    const users = await User.aggregate([
+      {
+        $match: {
+          _id: { $ne: userId },
+        },
+      },
+      { $sample: { size: 10 } },
+    ]);
+
+    const filtredUsers = users.filter(
+      (user) => !usersFollowedByMe.following.includes(user._id)
+    );
+    const suggestedUsers = filtredUsers.slice(0, 4);
+
+    suggestedUsers.forEach((user) => (user.password = null));
+
+    res.status(200).json(suggestedUsers);
+  } catch (error) {
+    console.log("Error in suggested users:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
