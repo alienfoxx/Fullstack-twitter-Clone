aa55e6f8d3a6f4ff751b9fe7e9251ca5306ba174
alienfoxx <alienfoxx001@gmail.com>
2025-02-17 11:45:24 +0100

    add the get allPosts functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -98,6 +98,20 @@ export const likeUnlikePost = async (req, res) => {
   }
 };
 
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+    if (posts.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getAllPosts controller:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 export const deletePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
